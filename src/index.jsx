90b7bd628d0912705711f5637df810dbfea40583
ussama585 <usama585@gmail.com>
2026-07-23 17:29:32 +0500

    commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
+import { SnackbarProvider } from 'notistack';
 
 // project imports
 import App from 'App';
@@ -19,17 +23,19 @@ const queryClient = new QueryClient();
 root.render(
   <QueryClientProvider client={queryClient}>
     <ConfigProvider>
-      <App />
+      <SnackbarProvider
+        maxSnack={3}
+        autoHideDuration={3000}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+      >
+        <App />
+      </SnackbarProvider>
     </ConfigProvider>
   </QueryClientProvider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
